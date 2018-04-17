f3a845d43406912d241401d868e40880d3235ef8
xinyu0721g <yanxinyu@umich.edu>
2018-04-16 23:24:57 -0400

    start building html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from model import *
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +8,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/search/')
+def search():
+    area_lst = get_old_region_dict(DB_NAME, lang='en')
+    area_lst.insert(0, 'ALL')
+    return render_template('search.html', area_lst=area_lst)
+
+
+@app.route('/search/data/', methods=["GET"])
+def search_data():
+    area_lst = request.args.get('area_lst')
+    print(area_lst)
+    area_result = []
+    for area in area_lst.split(','):
+        area_result += get_house_info_by_group(lang='en', group='region', group_name=area)
+    return json.dumps(area_result)
 
 
 @app.route('/cn')
@@ -19,3 +37,5 @@ def en():
     pass
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
